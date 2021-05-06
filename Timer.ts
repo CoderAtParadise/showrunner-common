ffb354375ef5258b5a96469887b80e7f3ad0f245
CoderAtParadise <dawn0966@gmail.com>
2021-05-06 11:50:34 +1000

    track timer state differently

--- Timer.ts
+++ Timer.ts
@@ -1,66 +1,89 @@
-import IJson from "./IJson"
-import {Point,stringify, parse,greaterThan,subtract,equals, now} from "./Time"
+import IJson from "./IJson";
+import {
+  Point,
+  stringify,
+  parse,
+  greaterThan,
+  subtract,
+  equals,
+  now,
+} from "./Time";
 
-export enum Behaviour
-{
-    STOP = "stop",
-    HIDE =  "hide",
-    OVERRUN = "overrun",
+export enum Behaviour {
+  STOP = "stop",
+  HIDE = "hide",
+  OVERRUN = "overrun",
 }
 
-export enum Display 
-{
-    COUNTDOWN = "countdown",
-    ELAPSED = "elapsed",
+export enum Display {
+  COUNTDOWN = "countdown",
+  ELAPSED = "elapsed",
 }
 
-export interface Settings
-{
-    duration : Point;
-    behaviour : Behaviour;
-    display : Display;
-    show : boolean;
+export interface Settings {
+  duration: Point;
+  behaviour: Behaviour;
+  display: Display;
+  show: boolean;
 }
 
-export interface Timer
-{
-    start: Point;
-    end: Point;
-    show: boolean;
-    overrun: boolean;
+export enum TimerState {
+  RUNNING = "running",
+  OVERRUN = "overrun",
+  STOPPED = "stopped",
+  HIDDEN = "hidden",
 }
 
-export function current(timer:Timer) : Point {
-    if(greaterThan(now(),timer.end))
-    {
-        timer.overrun = true;
-        return subtract(now(),timer.start);
-    }
-    return subtract(timer.end,now());
+export interface Timer {
+  start: Point;
+  end: Point;
+  state: TimerState;
 }
 
-export function isAt(tracking:Timer, time:Point) : boolean {
-    return equals(current(tracking),time);
-}
+export const TIMER_JSON: IJson<Timer> = {
+  serialize(value: Timer): object {
+    return {
+      start: stringify(value.start),
+      end: stringify(value.end),
+      state: value.state as TimerState,
+    };
+  },
+  deserialize(json: object): Timer {
+    const value = json as {
+      start: string;
+      end: string;
+      state: string;
+    };
+    return {
+      start: parse(value.start),
+      end: parse(value.end),
+      state: value.state as TimerState,
+    };
+  },
+};
 
-export const JSON : IJson<Settings> = {
-    serialize(value: Settings): object {
-        return {display: value.display as string, behaviour: value.behaviour as string, duration: stringify(value.duration),show: value.show};
-    },
+export const JSON: IJson<Settings> = {
+  serialize(value: Settings): object {
+    return {
+      display: value.display as string,
+      behaviour: value.behaviour as string,
+      duration: stringify(value.duration),
+      show: value.show,
+    };
+  },
 
-    deserialize(json:object): Settings {
-        const value = json as
-            {
-                display: string;
-                behaviour: string;
-                duration: string;
-                show: boolean;
-            };
-        return {
-            display: value.display as Display,
-            behaviour: value.behaviour as Behaviour,
-            duration: parse(value.duration),
-            show: value.show
-        };
-    },
-}
+  deserialize(json: object): Settings {
+    const value = json as {
+      display: string;
+      behaviour: string;
+      duration: string;
+      show: boolean;
+    };
+    return {
+      display: value.display as Display,
+      behaviour: value.behaviour as Behaviour,
+      duration: parse(value.duration),
+      show: value.show,
+    };
+  },
+};
